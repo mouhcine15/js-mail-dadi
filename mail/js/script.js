49118cf743133bebecd6ca002295894770e91571
mouhcine <mouhcine15.00@gmail.com>
2022-05-09 17:34:17 +0200

    js

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,21 @@
 const MyMailList=["Luca@gmail.com", "Marco@gmail.com", "Paolo@gmail.com"]; 
+
+const UserMail=document.querySelector("input");
+const btn=document.querySelector("button");
+const myOutput=document.querySelector("check");
+console.log("ciao");
+
+btn.addEventListener("click",
+    function () {
+
+        for (let i=0; i < MyMailList.length; i++) {
+
+            if (UserMail == MyMailList[0] || MyMailList [1] || MyMailList[2]) {
+                document.getElementsByClassName("check").innerHTML = ("Ci sei")
+            }
+
+        }
+
+    }
+
+)
